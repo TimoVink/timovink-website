ec0ed647689099fc2af9b991197b374915c03106
Timo Vink <timovink@gmail.com>
2022-06-04 15:01:26 -0700

    Simplify starter template. Add tailwind.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div className="h-screen flex justify-center items-center">
+        <p>Hello, world!</p>
+    </div>
+)
+
+export default App;
