31ac584615aee461b3ad433fb78d8fcd9f60a4ed
Timo Vink <timovink@gmail.com>
2022-08-20 17:27:01 -0700

    Add basic component for including code snippets from GitHub.

---
+++ web/src/data/GitHubContent.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { useApiCall } from 'queryClient';
+
+
+const GitHubContent = ({ repo, commit, branch, file, language, lines }) => {
+    const url = `https://raw.githubusercontent.com/${repo}/${commit}/${file}`;
+    const { isLoading, data } = useApiCall(url);
+
+    if (isLoading) {
+        return <pre>Loading...</pre>;
+    }
+
+    const lang = language || file.split('.').at(-1);
+    const langClass = `language-${lang}`;
+
+    let code = data;
+    if (lines && code) {
+        const [linesStart, linesEnd] = lines.split('-');
+        code = code
+            .split('\n')
+            .slice(linesStart - 1, linesEnd)
+            .join('\n');
+    }
+
+    return (
+        <pre>
+            <code className={langClass}>
+                {code}
+            </code>
+        </pre>
+    );
+};
+
+GitHubContent.propTypes = {
+    repo: PropTypes.string,
+    commit: PropTypes.string,
+    file: PropTypes.string.isRequired,
+    language: PropTypes.string,
+    lines: PropTypes.string,
+};
+
+GitHubContent.defaultProps = {
+    repo: 'TimoVink/tv-personalsite',
+    commit: 'main',
+};
+
+
+export default GitHubContent;
