a463eae6a4d22ef5ea0d78e039e65d2dbb60fa54
Timo Vink <timovink@gmail.com>
2022-09-05 20:43:31 -0700

    Add line numbers to code blocks.

--- web/src/data/SyntaxHighlightedCode.jsx
+++ web/src/data/SyntaxHighlightedCode.jsx
@@ -3,19 +3,29 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
-const SyntaxHighlightedCode = ({ className, ...props }) => {
+const SyntaxHighlightedCode = ({ className, startingLineNumber, children, ...props }) => {
     const match = /language-(\w+)/.exec(className || '');
     if (match) {
         return (
             <SyntaxHighlighter
                 language={match[1]}
                 style={stackoverflowDark}
-                PreTag={React.Fragment} {...props}
-            />
+                PreTag={React.Fragment}
+                startingLineNumber={startingLineNumber}
+                lineNumberStyle={{ 'opacity': 0.3 }}
+                showLineNumbers
+                {...props}
+            >
+                {children.trim()}
+            </SyntaxHighlighter>
         );
     }
 
-    return <code className={className} {...props} />;
+    return (
+        <code className={className} {...props}>
+            {children.trim()}
+        </code>
+    );
 }
 
 
