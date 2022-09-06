b896632ef7384baf1d444feeabfba4fe95be4606
Timo Vink <timovink@gmail.com>
2022-09-05 20:44:40 -0700

    Add links to the GitHub repo and file for GitHub-hosted code blocks.

--- web/src/data/GitHubContent.jsx
+++ web/src/data/GitHubContent.jsx
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types';
 import { useApiCall } from 'queryClient';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import SyntaxHighlightedCode from './SyntaxHighlightedCode';
 
 
-const GitHubContent = ({ repo, commit, branch, file, language, lines }) => {
+
+
+const GitHubContent = ({ repo, commit, file, language, lines }) => {
     const url = `https://raw.githubusercontent.com/${repo}/${commit}/${file}`;
     const { isLoading, data } = useApiCall(url);
+
+    const repoUrl = `https://github.com/${repo}/tree/${commit}`;
+    let fileUrl = `https://github.com/${repo}/blob/${commit}/${file}`;
 
     if (isLoading) {
         return <pre>Loading...</pre>;
@@ -19,20 +26,33 @@ const GitHubContent = ({ repo, commit, branch, file, language, lines }) => {
     }
 
     let code = data;
-    if (lines && code) {
-        const [linesStart, linesEnd] = lines.split('-');
+    let [linesStart, linesEnd] = [1, null];
+    if (lines) {
+        [linesStart, linesEnd] = lines.split('-').map(x => parseInt(x));
         code = code
             .split('\n')
             .slice(linesStart - 1, linesEnd)
             .join('\n');
+
+        fileUrl += `#L${linesStart}-L${linesEnd}`;
     }
 
     return (
-        <pre>
-            <SyntaxHighlightedCode className={langClass}>
-                {code}
-            </SyntaxHighlightedCode>
-        </pre>
+        <div>
+            <div className="flex items-center rounded-t-md px-4 py-2 bg-slate-900 space-x-2">
+                <a href={repoUrl} target="_blank" className="text-slate-200">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href={fileUrl} target="_blank" className="font-mono text-slate-200">
+                    {lines ? `${file}:${lines}` : file}
+                </a>
+            </div>
+            <pre className="rounded-t-none mt-0">
+                <SyntaxHighlightedCode className={langClass} startingLineNumber={linesStart}>
+                    {code}
+                </SyntaxHighlightedCode>
+            </pre>
+        </div>
     );
 };
 
